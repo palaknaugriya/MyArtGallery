182b3bcd4c0133ee8b77bb68f18e5562613f39bd
Palak <palak.gupta@mapmyindia.com>
2020-08-23 02:05:12 +0530

    Add welcome and gallery skeleton

--- models/Project.js
+++ models/Project.js
@@ -11,36 +11,36 @@
 // https://github.com/scottwrobinson/camo
 const Document = require('vertex-camo').Document
 const props = {
-  image: {type:String, default:''},
-  name: {type:String, default:'', trim:true, display:true},
-  category: {type:String, default:''},
-	slug: {type:String, default:'', immutable:true},
-  preview: {type:String, default:''},
-  link: {type:String, default:''},
-  client: {type:String, default:''},
-  dateRange: {type:String, default:''},
-  description: {type:String, default:'', isHtml:true},
-	schema: {type:String, default:'project', immutable:true},
-	dateString: {type:String, default:'', immutable:true},
-	timestamp: {type:Date, default: new Date(), immutable:true}
+    image: { type: String, default: '' },
+    name: { type: String, default: '', trim: true, display: true },
+    category: { type: String, default: '' },
+    slug: { type: String, default: '', immutable: true },
+    preview: { type: String, default: '' },
+    link: { type: String, default: '' },
+    client: { type: String, default: '' },
+    dateRange: { type: String, default: '' },
+    description: { type: String, default: '', isHtml: true },
+    schema: { type: String, default: 'project', immutable: true },
+    dateString: { type: String, default: '', immutable: true },
+    timestamp: { type: Date, default: new Date(), immutable: true }
 }
 
 class Project extends Document {
-	constructor(){
-		super()
-		this.schema(props)
+    constructor() {
+        super()
+        this.schema(props)
 
-		// this is how to set default values on new instances
-		this.timestamp = new Date()
-	}
+        // this is how to set default values on new instances
+        this.timestamp = new Date()
+    }
 
-	static get resourceName(){
-		return 'project'
-	}
+    static get resourceName() {
+        return 'project'
+    }
 
-	static collectionName(){
-			return 'projects'
-	}
+    static collectionName() {
+        return 'projects'
+    }
 }
 
 module.exports = Project
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
+const router = vertex.router()
+const projectController = require('../controllers/ProjectController')
 
 
 router.get('/', (req, res) => {
-    const data = {
-        greeting: 'Hi',
-        introduction: 'I am a backend devoloper'
-    }
-    res.render('landing', data);
+
+    const data = req.context //{...page, ...global}
+
+    const projectCtr = new projectController()
+    projectCtr.get()
+        .then(projects => {
+            data['projects'] = projects
+            res.render('landing', data);
+        })
+        .catch(err => {
+            res.send('Opps!' + err.message);
+        })
 });
 
 module.exports = router
